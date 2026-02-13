ed2a6129ec2a05b63632973d1cab7c9e740c7930
Joshua <joshua@danieland.net>
2026-02-12 22:13:33 -0500

    :)

--- LeetCode/7. Reverse Integer.py
+++ LeetCode/7. Reverse Integer.py
@@ -1,20 +1,11 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        #
+        # O(log n) O(log n)
         if x//10==0:
             return x
         sign=x//abs(x)
-        ans = int(str(abs(x))[::-1])*sign
-        if ans > -2e31 or ans < 2e31-1:
+        print(sign)
+        ans = int(str(x*sign)[::-1])*sign
+        if ans > -2147483648 and ans < 2147483647:
             return ans
         return 0
-
-s = Solution()
-print(s.reverse(1534236469))
-x = 9646324351
-if x > -2e31 and x < 2e31-1:
-    print(x)
-else: print(0)
-if x > -2147483648 and x < 2147483647:
-    print(x)
-else: print(0)
